41b7db7d48aad716e79c06a21c819c95617a91c2
Mohamed K. KONE <mkabir.kone@gmail.com>
2021-05-10 02:37:29 +0000

    added port to connection sheme

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const dbConfig = require("../config");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.database.name, dbConfig.database.user, dbConfig.database.password, {
   host: dbConfig.database.host,
+  port: dbConfig.database.port,
   dialect: dbConfig.database.dialect,
   operatorsAliases: '0',
 
@@ -19,9 +20,9 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.equipments = require("./gmao/equipment.js")(sequelize, Sequelize);
-db.equipmentTypes = require("./gmao/equipment_type.js")(sequelize, Sequelize);
-db.departments = require("./gmao/department.js")(sequelize, Sequelize);
-db.providers = require("./gmao/provider.js")(sequelize, Sequelize);
+db.equipments = require("./gmao/equipment")(sequelize, Sequelize);
+db.equipmentTypes = require("./gmao/equipment_type")(sequelize, Sequelize);
+db.departments = require("./gmao/department")(sequelize, Sequelize);
+db.providers = require("./gmao/provider")(sequelize, Sequelize);
 
 module.exports = db;
